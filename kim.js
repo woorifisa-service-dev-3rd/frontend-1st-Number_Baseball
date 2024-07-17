d02cce28249b6ac07ef6d312c89b0825360d9998
newgamer11 <qfpotw4u81@naver.com>
2024-07-18 08:50:10 +0900

    입력시 자동넘김 추가

--- kim.js
+++ kim.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    document.getElementById('number1').focus();
+};
+
 const inputs = document.querySelectorAll('input[id^="number"]');
 inputs.forEach((input, index) => {
     input.addEventListener('keyup', function (event) {
@@ -6,6 +10,19 @@ inputs.forEach((input, index) => {
         if (key === 'Backspace' || key === 'Delete') {
             if (index > 0) {
                 inputs[index - 1].focus();
+            }
+        }
+    });
+});
+
+const inputs2 = document.querySelectorAll('input[id^="number"]');
+inputs2.forEach((input, index) => {
+    input.addEventListener('keyup', function (event) {
+        const key = event.key;
+
+        if (key !== 'Backspace' && key !== 'Delete') {
+            if (index < 4) {
+                inputs[index + 1].focus();
             }
         }
     });
