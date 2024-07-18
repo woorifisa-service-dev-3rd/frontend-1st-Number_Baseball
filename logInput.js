d3ca74686368979567d355c13b05adb589501054
bangsk <cats7858@gmail.com>
2024-07-18 15:50:33 +0900

    성공 실패 팝업 추가

--- logInput.js
+++ logInput.js
@@ -1,41 +1,41 @@
-const log_box= document.getElementById('log_contain');
-const  answer_btn=document.getElementById('answer');
+const log_box = document.getElementById('log_contain');
+const answer_btn = document.getElementById('answer');
 
-const answer_1=document.getElementById('ex1').textContent;
-const answer_2=document.getElementById('ex2').textContent;
-const answer_3=document.getElementById('ex3').textContent;
-const answer_4=document.getElementById('ex4').textContent;
-const answer_list= [answer_1,answer_2,answer_3,answer_4];
+const answer_1 = document.getElementById('ex1').textContent;
+const answer_2 = document.getElementById('ex2').textContent;
+const answer_3 = document.getElementById('ex3').textContent;
+const answer_4 = document.getElementById('ex4').textContent;
+const answer_list = [answer_1, answer_2, answer_3, answer_4];
 
-answer_btn.addEventListener('click',()=>{
-    let logs= document.querySelectorAll('.logCount').length;
+answer_btn.addEventListener('click', () => {
+    let logs = document.querySelectorAll('.logCount').length;
 
-    if(logs<=9){
-        let out=4;
-        let strike=0;
-        let ball=0;
-        let first= document.getElementById('number1').value;
-        const second= document.getElementById('number2').value;
-        const third= document.getElementById('number3').value;
+    if (logs <= 9) {
+        let out = 4;
+        let strike = 0;
+        let ball = 0;
+        let first = document.getElementById('number1').value;
+        const second = document.getElementById('number2').value;
+        const third = document.getElementById('number3').value;
         const fourth = document.getElementById('number4').value;
-        
-        const input= [first,second,third,fourth];
-        for(let i=0;i<4;i++){
-            for(let j=0;j<4;j++){
-                if(input[i]==answer_list[j]){
-                    if(i==j){
+
+        const input = [first, second, third, fourth];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (input[i] == answer_list[j]) {
+                    if (i == j) {
                         strike++;
                     }
-                    else{
+                    else {
                         ball++;
                     }
                 }
             }
         }
 
-        out= out-strike-ball;
-        console.log(strike,ball,out);
-    
+        out = out - strike - ball;
+        console.log(strike, ball, out);
+
         let BText = '';
         if (ball !== 0) {
             BText = `${ball} B`;
@@ -63,21 +63,14 @@ answer_btn.addEventListener('click',()=>{
                     <strong>${OText}</strong>
                 </div>
             `;
-    
+
         log_box.appendChild(newBox);
-        document.getElementById('number1').value='';
-        document.getElementById('number2').value='';
-        document.getElementById('number3').value='';
-        document.getElementById('number4').value='';
+        document.getElementById('number1').value = '';
+        document.getElementById('number2').value = '';
+        document.getElementById('number3').value = '';
+        document.getElementById('number4').value = '';
 
         console.log(logs);
-        
-        if(logs==9){
-            
-
-        }
-    }
-   
-});
-
-
+    };
+}
+);
---
+++ popup.js
@@ -0,0 +1,14 @@
+const popup = document.getElementById('popup');
+const popupMessage = document.getElementById('popupMessage');
+const closePopupBtn = document.getElementById('closePopupBtn');
+
+
+// 팝업 닫기 버튼 클릭 시
+closePopupBtn.addEventListener('click', closePopup);
+
+// 팝업 외의 영역 클릭 시 팝업 닫기
+window.addEventListener('click', (e) => {
+    if (e.target === popup) {
+        closePopup();
+    }
+});
