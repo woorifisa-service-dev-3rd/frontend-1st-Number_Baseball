1d50594de87c76e223e457ddb3e759f246a9f1d1
newgamer11 <qfpotw4u81@naver.com>
2024-07-18 14:53:05 +0900

    키보드중복숫자방지 完-> 버튼 및 키보드 중복숫자방지 코드수정 예정

--- kim.js
+++ kim.js
@@ -28,38 +28,43 @@ inputs.forEach((input, index) => {
     });
 });
 
+// 키보드 입력시 중복숫자방지 31~65 line
+const input1 = document.getElementById('number1');
+const input2 = document.getElementById('number2');
+const input3 = document.getElementById('number3');
+const input4 = document.getElementById('number4');
+
 input1.addEventListener('input', () => {
-    b(1, 2, 3);
+    b(1, 2, 3, 0);
 })
 input2.addEventListener('input', () => {
-    b(0, 2, 3);
+    b(0, 2, 3, 1);
 })
 input3.addEventListener('input', () => {
-    b(0, 1, 3);
+    b(0, 1, 3, 2);
 })
 input4.addEventListener('input', () => {
-    b(0, 1, 2);
+    b(0, 1, 2, 3);
 })
 
-function b(a, b, c) {
+function b(a, b, c, d) {
     let keynum = ['', '', '', ''];
-
-    if (input1.value) {
-        keynum[0] = input1.value;
-    } else if (input2.value) {
-        keynum[1] = input1.value;
-    } else if (input3.value) {
-        keynum[2] = input1.value;
-    } else if (input4.value) {
-        keynum[3] = input1.value;
+    const inputs = document.querySelectorAll('input[id^=number]');
+    inputs.forEach((input, index) => {
+        keynum[index] = input.value;
+    })
+    if (inputs[d].value === keynum[a] || inputs[d].value === keynum[b] || inputs[d].value === keynum[c]) {
+        inputs[d].value = '';
     };
 
-    const inputs = document.querySelectorAll('input');
-    inputs.forEach(input => {
-        if (input.value === keynum[a] || input.value === keynum[b] || input.value === keynum[c]) {
-            input.value = '';
-        }
-    })
+
+
+    // const inputs = document.querySelectorAll('input[id^=number]');
+    // inputs.forEach(input => {
+    //     if (input.value === keynum[a] || input.value === keynum[b] || input.value === keynum[c]) {
+    //         input.value = '';
+    //     }
+    // })
 }
 
 
@@ -69,21 +74,18 @@ function b(a, b, c) {
 // 버튼 클릭 이벤트 처리
 
 
-const input1 = document.getElementById('number1');
-const input2 = document.getElementById('number2');
-const input3 = document.getElementById('number3');
-const input4 = document.getElementById('number4');
+// 키보드 중복방지 함수 입력을 위한 내용 위치 이동 -->31~34 line
 
-const btn1 = document.getElementById('btn_1');
-const btn2 = document.getElementById('btn_2');
-const btn3 = document.getElementById('btn_3');
-const btn4 = document.getElementById('btn_4');
-const btn5 = document.getElementById('btn_5');
-const btn6 = document.getElementById('btn_6');
-const btn7 = document.getElementById('btn_7');
-const btn8 = document.getElementById('btn_8');
-const btn9 = document.getElementById('btn_9');
-const btn0 = document.getElementById('btn_0');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+const btn0 = document.getElementById('btn0');
 
 const reset = document.getElementById('reset');
 const answer = document.getElementById('answer');
@@ -141,72 +143,7 @@ function disableAllButtonsExceptSelected() {
     });
 };
 
-//리셋버튼
-
-// function resetInputs() {
-//     var inputs = document.querySelectorAll('input[id^="number"]');
-//     inputs.forEach(input => {
-//         input.value = '';
-//     });
-//     document.getElementById('number1').focus();
-//     button.disabled = false;
-// }
-// const resetButton = document.getElementById('method_id2');
-// resetButton.addEventListener('click', resetInputs);
-
-
-
-/*
- 
-let selectedNumbers = ['', '', '', ''];
- 
-if(input1.value === selectNumbers[0]){
-    btn.disabled = false;
-}
- 
- 
-*/
-
-
-
-
-// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// let selectedNumbers = ['', '', '', ''];
-
-// input1.addEventListener('input', function () {
-//     handleInput(input1, 0);
-// });
-
-// input2.addEventListener('input', function () {
-//     handleInput(input2, 1);
-// });
-
-// input3.addEventListener('input', function () {
-//     handleInput(input3, 2);
-// });
-
-// input4.addEventListener('input', function () {
-//     handleInput(input4, 3);
-// });
-
-
-// function handleInput(input, index) {
-
-//     let value = input.value();
-
-//     if (selectedNumbers.includes(value)) {
-//         input.value = '';
-//         return;
-//     }
-
-//     selectedNumbers[index] = value;
-
-// };
-
-
-
-
+// 불필요 주석 제거
 
 btn1.addEventListener('click', () => {
     a(btn1);
@@ -272,29 +209,13 @@ function generateRandomNumbers() {
     return result;
 }
 
-function checkGuess(randomNumbers, userInput) {
-    let stlike = 0; // 맞은 숫자와 자리 모두 일치하는 개수 (Bulls)
-    let ball = 0;  // 맞은 숫자는 맞지만 자리는 다른 개수 (Cows)
+// 불필요 내용 제거 + 사용자가 입력한 숫자 배열을 가져오는 함수
+function getUserInput() {
+    return [
+        parseInt(input1.value),
+        parseInt(input2.value),
+        parseInt(input3.value),
+        parseInt(input4.value)
+    ];
 
-    for (let i = 0; i < randomNumbers.length; i++) {
-        if (randomNumbers[i] === userInput[i]) {
-            stlike++;
-        } else if (randomNumbers.includes(userInput[i])) {
-            ball++;
-        }
-    }
-
-    return { stlike, ball };
 }
-
-// 랜덤 숫자 생성
-const randomNumbers = generateRandomNumbers();
-console.log('Random Numbers:', randomNumbers);
-
-// 사용자 입력 (임의로 예시로 설정)
-const userInput = [1, 2, 3, 4]; // 사용자가 입력한 숫자 배열
-
-// 비교
-const result = checkGuess(randomNumbers, userInput);
-console.log('Bulls:', result.stlike); // Bulls 개수 출력
-console.log('Cows:', result.ball);   // Cows 개수 출력
